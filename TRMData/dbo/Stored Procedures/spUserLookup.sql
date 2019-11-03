7ed13151ed8fb9b4c1069ad0375ee83c9a0f6f14
Brian <brianj.pajak@gmail.com>
2019-11-03 13:55:20 -0500

    Getting User Data P1
    Return Data from logged in user

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128) 
+AS
+Begin
+	set nocount on;
+
+	select FirstName, LastName, EmailAddress, CreatedDate
+	from [dbo].[User]
+	where Id = @Id;
+End
---
+++ TRMDataManger.Library/DataAccess/UserData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManger.Library.Internal.DataAccess;
+using TRMDataManger.Library.Models;
+
+namespace TRMDataManger.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess(); // CRINGE!!!!!
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "TRMData"); // TODO change this
+
+            return output;
+
+        }
+    }
+}
