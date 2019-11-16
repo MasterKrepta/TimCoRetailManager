e2704915fee450d63f3c302bfdd1ac6f179c0091
Brian <brianj.pajak@gmail.com>
2019-11-16 11:52:13 -0500

    Add To Cart  Set up
    select items, adjust qty in stock and calculate subtotal.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -32,7 +32,7 @@ namespace TRMDesktopUI.ViewModels
         }
 
         private BindingList<ProductModel> _products;
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public BindingList<ProductModel> Products
         {
@@ -43,9 +43,23 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<string> _cart;
+        private ProductModel _selectedProduct;
 
-        public BindingList<string> Cart
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            { 
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set { 
@@ -61,7 +75,12 @@ namespace TRMDesktopUI.ViewModels
             get 
             {
                 //TODO replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -90,7 +109,8 @@ namespace TRMDesktopUI.ViewModels
             get { return _itemQuantity; }
             set { 
                 _itemQuantity = value; 
-                NotifyOfPropertyChange(() => ItemQuantity); 
+                NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -100,6 +120,10 @@ namespace TRMDesktopUI.ViewModels
             {
                 bool output = false;
                 //Check for selected and actual qty
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
                 return output;
             }
 
@@ -128,7 +152,37 @@ namespace TRMDesktopUI.ViewModels
 
         public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+
+                //TODO This is a hack: theres got to be a better way to refresh
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+
+                Cart.Add(item);
+            }
+      
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            
+
+        }
+
+        public void RemoveFromCart()
+        {
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public void Checkout()
